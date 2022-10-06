c98add143ce3a0ad88e8f328f86cfed670cccf28
Carole TOULORGE <carole.toulorge@hotmail.fr>
2022-10-06 16:36:26 +0200

    Intégration TP3 - Package declaration

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,19 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		byte a = 1;
+		short b = 2;
+		int c = 3;
+		long d = 4L;
+		float e = 5F;
+		double f = 6.4D;
+		char monChar = '!';
+		boolean myBool = true;
+		String monString = "Voici le résultat: \n1+5=6!";
+
+		System.out.println(monString);
+	}
+
+}
